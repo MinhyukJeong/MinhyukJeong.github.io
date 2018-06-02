7e1cb2d4e049b19e41d1c61d9aec9ad8db9b9ea8
MinhyukJeong <35296568+MinhyukJeong@users.noreply.github.com>
2018-06-03 05:15:10 +0900

    Delete popup.js

--- popup.js
+++
@@ -1,8 +0,0 @@
-
-const request = new XMLHttpRequest();
-const url = 'https://MinhyukJeong.github.io/setnumber.html'; 
-request.open('GET', url, true);
-request.onload = function () {
-  console.log(request.responseText); 
-};
-request.send();
